742603ef6eae26ebfa7741837ab8a24d7068620d
Вероника Зайцева <veronika@MacBook-Pro-Stanislav-2.local>
2021-11-14 20:41:44 +0300

    Added a slder and a burger menu

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const menuBurgerElem = document.querySelector('.burger-menu');
+const btnOpenBurgerElem = document.querySelector('.header__burger');
+const btnCloseBurgerElem = document.querySelector('.burger-menu__btn-close');
+
+const toogleOpened = (elem) => {
+    elem.classList.toggle('is-opened');
+}
+
+const openBurger = () => {
+    toogleOpened(menuBurgerElem);
+    document.body.classList.add('is-opened-modal');
+}
+
+const closeBurger = () => {
+    toogleOpened(menuBurgerElem);
+    document.body.classList.remove('is-opened-modal');
+}
+
+btnOpenBurgerElem.addEventListener('click', openBurger);
+btnCloseBurgerElem.addEventListener('click', closeBurger);
